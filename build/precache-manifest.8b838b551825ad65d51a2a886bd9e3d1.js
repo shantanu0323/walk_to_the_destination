2c0eb35dd8b8313bf84fdb362a2b28b606bf94c4
Shantanu Pramanik <srs.shaan@gmail.com>
2020-08-29 02:26:23 +0530

    [Build] Push

---
+++ build/precache-manifest.8b838b551825ad65d51a2a886bd9e3d1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "950665b7c37dce9d5b03b33d131b42a7",
+    "url": "/walk_to_the_destination/index.html"
+  },
+  {
+    "revision": "f4f72ed689b1460438f1",
+    "url": "/walk_to_the_destination/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "413d5a21f5a0a390f045",
+    "url": "/walk_to_the_destination/static/css/main.a536b6e7.chunk.css"
+  },
+  {
+    "revision": "f4f72ed689b1460438f1",
+    "url": "/walk_to_the_destination/static/js/2.e3f0b813.chunk.js"
+  },
+  {
+    "revision": "a8eb76e4c6beb72c22bcc9f3c026930b",
+    "url": "/walk_to_the_destination/static/js/2.e3f0b813.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "413d5a21f5a0a390f045",
+    "url": "/walk_to_the_destination/static/js/main.1358ab02.chunk.js"
+  },
+  {
+    "revision": "ccb25d65120a3d590217",
+    "url": "/walk_to_the_destination/static/js/runtime-main.6e6330b8.js"
+  },
+  {
+    "revision": "531425795ea1fc7812468289c1d29934",
+    "url": "/walk_to_the_destination/static/media/bg-img.53142579.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/walk_to_the_destination/static/media/logo.5d5d9eef.svg"
+  }
+]);
