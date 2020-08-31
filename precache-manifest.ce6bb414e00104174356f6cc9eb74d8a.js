f73a9304b053adff945a2358347a6650111a01c3
Shantanu Pramanik <srs.shaan@gmail.com>
2020-08-31 18:55:50 +0530

    Updates

--- precache-manifest.ce6bb414e00104174356f6cc9eb74d8a.js
+++ precache-manifest.ce6bb414e00104174356f6cc9eb74d8a.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "74ece86145b02f41bd27e38b0a45140c",
+    "revision": "e0df03bc2ff822c468361a9be75e9984",
     "url": "/walk_to_the_destination/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/walk_to_the_destination/static/css/2.f3cffc9e.chunk.css"
   },
   {
-    "revision": "4bfe3c46a8b0d10f1df8",
-    "url": "/walk_to_the_destination/static/css/main.f096f8ec.chunk.css"
+    "revision": "6c71b850a73acb6efa11",
+    "url": "/walk_to_the_destination/static/css/main.6eb63c31.chunk.css"
   },
   {
     "revision": "45fd33c03d98d8c5b6b6",
@@ -20,16 +20,16 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/walk_to_the_destination/static/js/2.088318b5.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "4bfe3c46a8b0d10f1df8",
-    "url": "/walk_to_the_destination/static/js/main.a887afc0.chunk.js"
+    "revision": "6c71b850a73acb6efa11",
+    "url": "/walk_to_the_destination/static/js/main.db3908f8.chunk.js"
   },
   {
     "revision": "ccb25d65120a3d590217",
     "url": "/walk_to_the_destination/static/js/runtime-main.6e6330b8.js"
   },
   {
-    "revision": "531425795ea1fc7812468289c1d29934",
-    "url": "/walk_to_the_destination/static/media/bg-img.53142579.jpg"
+    "revision": "886cc6f72cc8939d47be3072d9aa3f73",
+    "url": "/walk_to_the_destination/static/media/bg-img.886cc6f7.jpg"
   },
   {
     "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/walk_to_the_destination/precache-manifest.629f8243275cfa35f8e194c2074017bb.js"
+  "/walk_to_the_destination/precache-manifest.ce6bb414e00104174356f6cc9eb74d8a.js"
 );
 
 self.addEventListener('message', (event) => {
